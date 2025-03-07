c5bbf81a46a061a40d7503b3dd81ea715128098f
uncldrw <dreq2905@gmail.com>
2025-03-07 18:49:27 +0100

    Füge Supabase-Client hinzu und aktualisiere das Styling der Header- und Sidebar-Komponenten

--- components/CanvasSidebar.vue
+++ components/CanvasSidebar.vue
@@ -11,17 +11,18 @@ const items = ref([
 </script>
 
 <template>
-  <aside class="w-20 h-full">
-    <div class="border p-3 bg-stone-800 border-stone-600/40 h-full rounded-lg">
-      <div class="py-0">
-        <ul class="grid gap-2">
+  <aside class="sidebar">
+    <div class="sidebar__container">
+      <div class="sidebar__content">
+        <ul class="sidebar__list">
           <template v-for="item in items">
-            <li class="inline-flex aspect-square w-full">
-              <button
-                class="rounded-md hover:bg-stone-700/40 w-full inline-grid place-content-center cursor-pointer"
-              >
-                <Icon class="size-6 text-stone-50" :name="item.icon" />
+            <li class="sidebar__item">
+              <button class="sidebar__button">
+                <Icon class="sidebar__icon" :name="item.icon" />
               </button>
+              <span class="sidebar__item-flyout">
+                {{ item.title }}
+              </span>
             </li>
           </template>
         </ul>
@@ -29,3 +30,49 @@ const items = ref([
     </div>
   </aside>
 </template>
+
+<style lang="postcss">
+@reference "../assets/css/main.postcss";
+.sidebar {
+  @apply w-14 h-full;
+}
+
+.sidebar__container {
+  @apply border p-2 bg-stone-800 border-stone-600/40 h-full rounded-lg;
+}
+
+.sidebar__content {
+  @apply py-0;
+}
+
+.sidebar__list {
+  @apply grid gap-2;
+}
+
+.sidebar__item {
+  @apply inline-flex aspect-square w-full relative;
+}
+
+.sidebar__button {
+  @apply rounded-md w-full inline-grid place-content-center cursor-pointer  transition-all duration-100;
+  &:hover {
+    @apply bg-stone-700/40;
+    .sidebar__icon {
+      @apply scale-115 text-stone-100;
+    }
+  }
+}
+
+.sidebar__icon {
+  @apply size-5 text-stone-400 transition-transform duration-100;
+}
+
+.sidebar__item-flyout {
+  @apply bg-indigo-600 text-white absolute top-1/2 left-[calc(100%+0.5rem+5px)] -translate-y-1/2 px-2 py-1 rounded-md text-sm font-medium  transition-all duration-300 opacity-0;
+  /* transform-origin: left; */
+}
+
+.sidebar__item:hover .sidebar__item-flyout {
+  @apply opacity-100 delay-200 duration-75;
+}
+</style>
