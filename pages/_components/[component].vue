6bd7c89636fb28bcdc9f6dce8687634ed4266f13
uncldrw <dreq2905@gmail.com>
2025-03-09 19:56:00 +0100

    Füge neue Komponente hinzu, die basierend auf Route-Parametern dynamisch einen Button oder eine Nachricht anzeigt

---
+++ pages/_components/[component].vue
@@ -0,0 +1,10 @@
+<script></script>
+
+<template>
+  <div class="w-screen h-screen grid place-content-center">
+    <template v-if="$route.params.component">
+      <component :is="'Button'">Hi</component></template
+    >
+    <p v-else>Keine Komponente</p>
+  </div>
+</template>
