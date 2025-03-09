c161d0d6b11c04d8026c76c4a1aebc26f9022f07
uncldrw <dreq2905@gmail.com>
2025-03-09 19:55:57 +0100

    Ersetze Button durch AppButton und aktualisiere Platzhaltertext im Eingabefeld

--- pages/test.vue
+++ pages/test.vue
@@ -2,8 +2,8 @@
   <div class="w-screen h-screen grid place-content-center">
     <div class="flex gap-4">
       <h1 class="text-4xl font-bold text-white">Lea ist Penis</h1>
-      <Button icon="ph:share-network">Absenden</Button>
-      <Input placeholder="test" />
+      <AppButton icon="ph:share-network">Absenden</AppButton>
+      <Input placeholder="Name" />
     </div>
   </div>
 </template>
