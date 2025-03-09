b01db7a194dc40d5747a47a4c6f92dd15cbd64cd
Jason <dackinhluan@icloud.com>
2025-03-09 16:19:15 +0100

    Füge neue Testseite mit Button und Input-Komponente hinzu

---
+++ pages/test.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-screen h-screen grid place-content-center">
+    <div class="flex gap-4">
+      <Button icon="ph:share-network-bold">Absenden</Button>
+      <Input placeholder="test" />
+    </div>
+  </div>
+</template>
