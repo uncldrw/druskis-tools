26f977d5129ccd8d29db8d6bf58cab881aca2c68
Jason <dackinhluan@icloud.com>
2025-03-09 16:19:13 +0100

    Füge neue Input-Komponente hinzu mit Platzhalter und Zwei-Wege-Datenbindung

---
+++ components/Input.vue
@@ -0,0 +1,55 @@
+<script setup>
+const props = defineProps({
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  modelValue: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+function onInput(event) {
+  emit("update:modelValue", event.target.value);
+}
+</script>
+
+<template>
+  <span class="appInput">
+    <input placeholder="" type="text" :value="modelValue" @input="onInput" />
+    <span
+      v-if="placeholder.length"
+      class="placeholder"
+      :class="{ 'has-value': modelValue.length > 0 }"
+    >
+      {{ placeholder }}
+    </span>
+  </span>
+</template>
+
+<style lang="postcss">
+@reference "../assets/css/main.postcss";
+
+.appInput {
+  @apply border rounded-md border-neutral-500 relative inline-block;
+
+  input {
+    @apply text-white text-sm outline-none px-3 py-2.5 bg-transparent w-full;
+  }
+
+  .placeholder {
+    @apply absolute left-3 top-1/2 transform -translate-y-1/2 text-sm text-neutral-400 pointer-events-none transition-all opacity-100;
+
+    &.has-value {
+      @apply text-xs top-0 -translate-y-1/2 text-neutral-300;
+    }
+  }
+
+  input:not(:placeholder-shown) + .placeholder {
+    @apply opacity-0;
+  }
+}
+</style>
